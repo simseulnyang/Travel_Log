05bd131018a9781a22f537835a4a46318fec8c2f
simseulnyang <happysseul627@gmail.com>
2023-07-18 23:56:29 +0900

    회원가입, 로그인 form 코드 작성

---
+++ user/forms.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import get_user_model
+from django import forms
+User = get_user_model()
+
+
+class RegisterForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ['email', 'nickname']
+
+
+class LoginForm(AuthenticationForm):
+    # email = forms.EmailField(widget=forms.EmailInput(
+    #     attrs={'placeholder': 'email을 입력해주세요.', 'style': 'border : 1px solid #ddd'}), label='Email')
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={'placeholder': 'Password를 입력해주세요.', 'style': 'border : 1px solid #ddd'}), label='Password')
+
+    class Meta:
+        model = User
+        fields = ['email', 'password']
