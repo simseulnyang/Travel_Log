04ac063d201e708aee7ce9d9f6cbde90a8e82d52
simseulnyang <happysseul627@gmail.com>
2023-07-18 23:52:42 +0900

    글목록, 글작성, 상세페이지조회, 수정 url 작성

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,15 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
+    # 글 목록
     path('', views.PostList.as_view(), name='list'),
+
+    # 글 작성
+    path('write/', views.PostWrite.as_view(), name='write'),
+
+    # 글 상세페이지 조회
+    path('detail/<int:pk>/', views.PostDetail.as_view(), name='detail'),
+
+    # 글 수정
+    path('detail/<int:post_id>/edit/', views.PostUpdate.as_view(), name='edit'),
 ]
