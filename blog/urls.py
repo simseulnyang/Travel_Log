e4a20636273fbacd3580bee88a3dafe430d9f011
simseulnyang <happysseul627@gmail.com>
2023-07-19 18:09:12 +0900

    comment, hashtag관련 url 설정

--- blog/urls.py
+++ blog/urls.py
@@ -17,5 +17,22 @@ urlpatterns = [
     path('detail/<int:pk>/edit/', views.PostUpdate.as_view(), name='edit'),
 
     # 글 삭제
-    path('detail/<int:pk>/delete/', views.PostDelete.as_view(), name='delete'),
+    path('detail/<int:pk>/delete/',
+         views.PostDelete.as_view(), name='delete'),
+
+    # 코멘트 작성
+    path("detail/<int:pk>/comment/write/",
+         views.CommentWrite.as_view(), name='cm-write'),
+
+    # 코멘트 삭제
+    path("detail/comment/<int:pk>/delete/",
+         views.CommentDelete.as_view(), name='cm-delete'),
+
+    # 태그 작성
+    path("detail/<int:pk>/hashtag/write/",
+         views.HashTagWrite.as_view(), name='tag-write'),
+
+    # 태그 삭제
+    path("detail/<int:pk>/hashtag/delete/",
+         views.HashTagDelete.as_view(), name='tag-delete'),
 ]
