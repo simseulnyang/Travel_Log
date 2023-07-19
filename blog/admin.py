962e5251e2203a9286728d27b7e45050d95eeb65
simseulnyang <happysseul627@gmail.com>
2023-07-19 18:08:22 +0900

    admin에서 comment, hashtag 관리

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment, HashTag
 
 # Register your models here.
+
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(HashTag)
+class HashTagAdmin(admin.ModelAdmin):
     pass
