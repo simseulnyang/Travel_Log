5b9bf49eb297d6f7107fc6a182cc591c0cb1ecef
simseulnyang <happysseul627@gmail.com>
2023-07-25 00:14:40 +0900

    admin에 category 추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from .models import Post, Comment, HashTag
+from .models import Post, Category, Comment, HashTag
 
 # Register your models here.
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
     pass
 
 
