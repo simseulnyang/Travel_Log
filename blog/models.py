05a78804dc2c91a8fd1a1d5f4aae2de6bf87f819
simseulnyang <happysseul627@gmail.com>
2023-07-24 17:46:25 +0900

    category model 추가

--- blog/models.py
+++ blog/models.py
@@ -7,12 +7,23 @@ User = get_user_model()
 
 
 class Post(models.Model):
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     content = models.TextField()
     writer = models.ForeignKey(User, on_delete=models.CASCADE)
     hits = models.PositiveIntegerField(default=0, verbose_name='조회수')
+    is_deleted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
 
 
 class Comment(models.Model):
