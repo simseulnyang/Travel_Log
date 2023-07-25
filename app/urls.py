1eb939cb577e413df44a0906dbad4c6554e0a087
simseulnyang <happysseul627@gmail.com>
2023-07-25 16:04:13 +0900

    media file을 위한 urlpattern 설정

--- app/urls.py
+++ app/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from . import views
@@ -24,3 +26,8 @@ urlpatterns = [
     path('user/', include('user.urls')),
     path('', views.IndexMain.as_view(), name='index')
 ]
+
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+)
