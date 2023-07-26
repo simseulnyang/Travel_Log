7c745c37ee86ade286491cf00b31e4811cd1ac6e
simseulnyang <happysseul627@gmail.com>
2023-07-26 22:18:51 +0900

    프로필작성 url 추가

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'user'
 
@@ -14,4 +16,7 @@ urlpatterns = [
     # 로그아웃
     path('logout/', views.Logout.as_view(), name='logout'),
 
-]
+    # 프로필작성
+    path('profile/edit/', views.ProfileWrite.as_view(), name="pf-write"),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
